72333d79fb3b407bd1ff83aa7d3f44dc1ee01f41
Gabimaru <goukilya5@gmail.com>
2024-10-23 22:22:40 +0700

    feat: Сделал новый блок с аккордеоном

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-const headerButton = document.querySelector('.header__button'),
+const body = document.querySelector('.body'),
+    header = document.querySelector('.header'),
+    headerButton = document.querySelector('.header__button'),
     thirdLine = headerButton.querySelector('.third-line');
 
 headerButton.addEventListener('click', () => {
     headerButton.classList.toggle('width-line')
+    header.classList.toggle('header_active')
+    body.classList.toggle('lock-screen')
 })
+
+
+
+
+const language = document.querySelector('.language');
+
+document.addEventListener('click', e => {
+    if (e.target == language) {
+        language.classList.toggle('open')
+    } else if (language.classList.contains('open')) {
+        language.classList.remove('open')
+    }
+})
+
+
+const accordions = document.querySelectorAll('.accordion'),
+    titles = document.querySelectorAll('.accordion__title'),
+    contents = document.querySelectorAll('.accordion__content');
+
+accordions.forEach(item => item.addEventListener('click', () => {
+    const activeContent = document.querySelector('#' + item.dataset.tab)
+
+    if (activeContent.classList.contains('content__active')) {
+        activeContent.classList.remove('content__active')
+        item.classList.remove('accordion__active')
+        activeContent.style.maxHeight = 0
+    } else {
+        contents.forEach(element => {
+            element.classList.remove('content__active')
+            element.style.maxHeight = 0
+        })
+
+        accordions.forEach(
+            element => element.classList.remove('accordion__active')
+        )
+
+        item.classList.add('accordion__active')
+        activeContent.classList.add('content__active')
+        activeContent.style.maxHeight = activeContent.scrollHeight + 'px'
+    }
+}))
+
+document.querySelector('[data-tab="tab-1"]').classList.add('accordion__active')
+document.querySelector('#tab-1').classList.add('content__active')
+document.querySelector('#tab-1').style.maxHeight = document.querySelector('#tab-1').scrollHeight + 'px'
