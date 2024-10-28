0ff27277e5472aa3057e1d125e357de5b78cd66a
Gabimaru <goukilya5@gmail.com>
2024-10-28 07:28:51 +0700

    Updates

---
+++ swiper/swiper.js
@@ -0,0 +1,23 @@
+new Swiper('.swiper-first', {
+    loop: true,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
+
+new Swiper('.swiper-second', {
+    effect: 'cards',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    keyboard: {
+        enabled: true
+    }
+});
